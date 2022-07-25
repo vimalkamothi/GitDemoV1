624a0dfc077b65260f4f283977ed9bc9c957f7a0
vimal <kamothi.vimal@gmail.com>
2022-07-25 13:46:59 +0100

    Committed expected review comments

--- src/GitDemo/Test2.java
+++ src/GitDemo/Test2.java
@@ -4,7 +4,8 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test 2");
+		System.out.println("Test 2 - 3 - 4");
+		System.out.println("Test 5 - 6 - 7");
 	}
 
 }
